0fb41bb09a044924fb0c65649e98c6fc75d20cd6
Erik Olofsson <erik@olofsson.info>
2016-08-06 10:48:06 +0200

    [Malterlib/Daemon] Remove pure define

--- Source/Platform/Malterlib_Daemon_PlatformImp_Linux_Interface.h
+++ Source/Platform/Malterlib_Daemon_PlatformImp_Linux_Interface.h
@@ -13,13 +13,13 @@ namespace NMib
 		public:
 			virtual ~CServiceSystemInterface() {};
 
-			virtual EActionResult f_Start(CServiceParams const &_Params) pure;
-			virtual EActionResult f_Stop(CServiceParams const &_Params, bint _bWait = false) pure;
+			virtual EActionResult f_Start(CServiceParams const &_Params) = 0;
+			virtual EActionResult f_Stop(CServiceParams const &_Params, bint _bWait = false) = 0;
 
-			virtual EActionResult f_Add(CServiceParams const &_Params, bint _bCheckForExisting = false) pure;
-			virtual EActionResult f_Remove(CServiceParams const &_Params) pure;
+			virtual EActionResult f_Add(CServiceParams const &_Params, bint _bCheckForExisting = false) = 0;
+			virtual EActionResult f_Remove(CServiceParams const &_Params) = 0;
 
-			virtual EActionResult f_Exists(CServiceParams const &_Params, bool &_bExists) const pure;
+			virtual EActionResult f_Exists(CServiceParams const &_Params, bool &_bExists) const = 0;
 		};
 		
 	} // namespace NService
